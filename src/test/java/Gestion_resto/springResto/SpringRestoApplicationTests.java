a46750dd8813590b3ab6a8ac89ee259baf891919
TalibeCheikhCodeur <tidjanely99@gmail.com>
2024-05-05 15:36:27 +0000

    initialisation projet resto odc

---
+++ src/test/java/Gestion_resto/springResto/SpringRestoApplicationTests.java
@@ -0,0 +1,13 @@
+package Gestion_resto.springResto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestoApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
